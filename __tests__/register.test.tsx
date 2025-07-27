6e112462e22a523368db04e4eb8f787985b442cd
boterop <boterop22@gmail.com>
2025-07-27 13:04:32 -0500

    Fix tests

--- __tests__/register.test.tsx
+++ __tests__/register.test.tsx
@@ -1,17 +1,18 @@
 import { fireEvent, render } from "@testing-library/react-native";
 import { router } from "expo-router";
 import RegisterScreen from "@/app/register";
+import en from "@/lang/locales/en.json";
 
 describe("RegisterScreen", () => {
   it("renders correctly", () => {
-    const expectedText = "Registro";
+    const expectedText = en.register_login;
     const component = render(<RegisterScreen />);
 
     expect(component.getByText(expectedText)).toBeTruthy();
   });
 
   it("navigates to login screen", () => {
-    const buttonText = "¿Ya tienes cuenta? Iniciar sesión";
+    const buttonText = en.register_login;
     const component = render(<RegisterScreen />);
     const element = component.getByText(buttonText);
 
