164b523eba53ce477d4f0ac219373c329a40b5c0
boterop <boterop22@gmail.com>
2025-08-24 17:24:41 -0500

    Set text color

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -3,6 +3,7 @@ import { TextInput } from "react-native";
 const Input = (props: any) => (
   <TextInput
     {...props}
+    style={[props.style, tw`text-black`]}
     placeholderTextColor={props.placeholderTextColor || "#9CA3AF"}
   />
 );
