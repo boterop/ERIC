bbef0966b5df1e60be47839fa77c4e5dbe070150
boterop <boterop22@gmail.com>
2025-09-23 12:15:02 -0500

    Do not persist answers

--- adapters/testAnswerApi.ts
+++ adapters/testAnswerApi.ts
@@ -1,47 +1,29 @@
+import { Answer } from "@/domain/Answer";
 import { AnswerApi } from "@/ports";
-import { storageService } from "@/adapters/storageService";
+
+const answers: Answer[] = [];
 
 export const testAnswerApi: AnswerApi = {
   get: async (id, _token) => {
-    const response = await storageService.get(`answer-${id}`);
+    const response = answers.find((a) => a.id === id);
 
-    return JSON.parse(response || "");
+    if (!response) throw new Error("Not found");
+
+    return response;
   },
   create: async (answer, _token) => {
-    await storageService.save(`answer-${answer.id}`, JSON.stringify(answer));
-    const dimension = await storageService.get(`answer-${answer.dimension}`);
-    const parsed = JSON.parse(dimension || "[]");
-    parsed.push(answer);
-    await storageService.save(
-      `answer-${answer.dimension}`,
-      JSON.stringify(parsed),
-    );
-
+    answers.push(answer);
     return answer;
   },
   update: async (answer, _token) => {
-    await storageService.save(
-      `answer-${answer.id}`,
-      JSON.stringify({ answer }),
-    );
-    const dimension = await storageService.get(`answer-${answer.dimension}`);
-    const newDimension = JSON.parse(dimension || "[]").filter(
-      (ans: any) => ans.id !== answer.id,
-    );
-    newDimension.push(answer);
-    await storageService.save(
-      `answer-${answer.dimension}`,
-      JSON.stringify(answer),
-    );
-    await storageService.save(
-      `answer-${answer.dimension}`,
-      JSON.stringify(newDimension),
-    );
+    const index = answers.findIndex((a) => a.id === answer.id);
+
+    if (index === -1) throw new Error("Not found");
+
+    answers[index] = answer;
 
     return answer;
   },
-  listByDimension: async (dimension, _token) => {
-    const response = await storageService.get(`answer-${dimension}`);
-    return JSON.parse(response || "[]");
-  },
+  listByDimension: async (dimension, _token) =>
+    answers.filter((a) => a.dimension === dimension),
 };
