0a0ab99734ea816fe0206489d31eac059eb94aa3
Santiago Botero <98826652+boterop@users.noreply.github.com>
2026-06-23 10:35:03 -0500

    Create a mock for user api (#43)

--- services/userService.ts
+++ services/userService.ts
@@ -1,12 +1,24 @@
 import { userApi } from "@/adapters/userApi";
+import { testUserApi } from "@/adapters/testUserApi";
 import { User } from "@/domain/User";
 
 const userService = {
-  login: async (email: string, password: string) =>
-    userApi.login(email, password),
-  register: async (user: User) => userApi.register(user),
-  me: async (token: string) => userApi.me(token),
-  students: async (token: string) => userApi.students(token),
+  login: async (email: string, password: string) => {
+    const adapter = process.env.USER === "test" ? testUserApi : userApi;
+    return adapter.login(email, password);
+  },
+  register: async (user: User) => {
+    const adapter = process.env.USER === "test" ? testUserApi : userApi;
+    return adapter.register(user);
+  },
+  me: async (token: string) => {
+    const adapter = process.env.USER === "test" ? testUserApi : userApi;
+    return adapter.me(token);
+  },
+  students: async (token: string) => {
+    const adapter = process.env.USER === "test" ? testUserApi : userApi;
+    return adapter.students(token);
+  },
 };
 
 export default userService;
